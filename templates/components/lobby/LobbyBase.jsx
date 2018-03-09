2bfd528a5d6b8b6ddd5fa7863ba631bbb3ae63d7
Matthew Rimmer <36701988+Matt-Rimmer@users.noreply.github.com>
2018-03-09 10:39:46 +0000

    Update LobbyBase.jsx
    Copied from tutorial, as a starting point and template for future work.

--- templates/components/lobby/LobbyBase.jsx
+++ templates/components/lobby/LobbyBase.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Websocket from 'react-websocket'
 import $ from 'jquery'
+import PlayerGames from './PlayerGames'
 
 class LobbyBase extends React.Component {
 
@@ -16,7 +17,17 @@ class LobbyBase extends React.Component {
         this.sendSocketMessage = this.sendSocketMessage.bind(this);
     }
 
-    componentDidMount() {}
+    getPlayerGames(){
+        this.serverRequest = $.get('http://localhost:8080/player-games/?format=json', function (result) {
+           this.setState({
+            player_game_list: result,
+             })
+        }.bind(this))
+    }
+
+    componentDidMount() {
+       this.getPlayerGames()
+    }
 
     componentWillUnmount() {
         this.serverRequest.abort();
@@ -25,8 +36,8 @@ class LobbyBase extends React.Component {
     handleData(data) {
         //receives messages from the connected websocket
         let result = JSON.parse(data)
-        
-        
+        // new games, so get an updated list of this player's game
+        this.getPlayerGames()
         // we've received an updated list of available games
         this.setState({available_game_list: result})
     }
@@ -43,7 +54,10 @@ class LobbyBase extends React.Component {
             <div className="row">
                 <Websocket ref="socket" url={this.props.socket}
                     onMessage={this.handleData.bind(this)} reconnect={true}/>
-                <span>Lobby Components will go here....</span>
+                <div className="col-lg-4">
+                    <PlayerGames player={this.props.current_user} game_list={this.state.player_game_list}
+                                 sendSocketMessage={this.sendSocketMessage} />
+                </div>
             </div>
 
         )
