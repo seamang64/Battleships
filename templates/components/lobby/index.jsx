481fa5c215d317d55c339309be2170f55d88f337
Cameron <36801227+CameronB66@users.noreply.github.com>
2018-04-06 15:07:00 +0100

    Game working (minus auto update)

---
+++ templates/components/lobby/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import LobbyBase from './LobbyBase.jsx'
+import ReactDOM from 'react-dom'
+import $ from 'jquery'
+
+// lobby socket url
+var lobby_sock = 'ws://' + window.location.host + "/ws/lobby/"
+// preset the current_user
+var current_user = null
+
+// renders out the base component
+
+$.get('http://127.0.0.1:8080/current-user/?format=json', function(result){
+     //gets the current user information from Django
+    current_user = result
+    render_component()
+})
+
+function render_component(){
+    ReactDOM.render(<LobbyBase current_user={current_user} socket={lobby_sock}/>, document.getElementById('lobby_component'))
+}
