4e193ad6aa2938c684b71d7fbe3cce9979b60d71
Cameron <36801227+CameronB66@users.noreply.github.com>
2018-04-06 15:02:04 +0100

    Game working (minus auto update)

--- battleships/routing.py
+++ battleships/routing.py
@@ -1,4 +1,14 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from game import routing
 
-application = ProtocolTypeRouter({})
+from django.conf.urls import url
+
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
--- battleships/urls.py
+++ battleships/urls.py
@@ -1,28 +1,28 @@
-"""battleships URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+# battleships/urls.py
+from django.conf.urls import url, include
 from django.contrib import admin
-from game.views import *
+from game.views import __init__, api_views, views
 from django.contrib.auth.views import login, logout
+from rest_framework.routers import DefaultRouter
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^register/', CreateUserView.as_view()),
+    url(r'^register/', views.CreateUserView.as_view()),
     url(r'^login/$', login, {'template_name': 'login.html'}),
     url(r'^logout/$', logout, {'next_page': '/'}),
- 
-    url(r'^$', HomeView.as_view())
+    url(r'^lobby/$', views.LobbyView.as_view()),
+    url(r'^$', views.HomeView.as_view()),
+	url(r'^game/(?P<game_id>\d+)/$', views.GameView.as_view()),
 ]
+
+urlpatterns += [
+ url(r'^current-user/', api_views.CurrentUserView.as_view()),
+ url(r'^game-from-id/(?P<game_id>\d+)/$', api_views.SingleGameViewSet.as_view()),
+]
+
+router = DefaultRouter()
+router.register(r'player-games', api_views.PlayerGameViewSet, 'player_games')
+router.register(r'available-games', api_views.AvailableGameViewSet, 'available_games')
+
+urlpatterns += router.urls
